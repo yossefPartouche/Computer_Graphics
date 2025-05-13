eae41dee03b5a8d2b22d0d7007c95a71223224ea
talsolovey <168472280+talsolovey@users.noreply.github.com>
2025-05-13 18:33:17 +0300

    scene 1 working

--- EX03/hw3.py
+++ EX03/hw3.py
@@ -16,29 +16,35 @@ def render_scene(camera, ambient, lights, objects, screen_size, max_depth):
             direction = normalize(pixel - origin)
             ray = Ray(origin, direction)
 
-            t, object = ray.nearest_intersected_object(objects)
-            hitP = ray.origin + t * ray.direction
+            obj_intersect = ray.nearest_intersected_object(objects)
 
-            color = np.zeros(3)
-            #color = (255,255,255)
+            if obj_intersect is None:
+                color = np.zeros(3)
+
+            else:
+                t, obj = obj_intersect
+                hitP = ray.origin + t * ray.direction
            
-            # This is the main loop where each pixel color is computed.
-            for c in range(3):
-                color[c] = getColor(c, ambient, object, objects, lights, ray, hitP, max_depth, level=1)
+                # This is the main loop where each pixel color is computed.
+                color = getColor(ambient, obj, objects, lights, ray, hitP, max_depth, 1)
             
             # We clip the values between 0 and 1 so all pixel values will make sense.
             image[i, j] = np.clip(color,0,1)
 
     return image
 
-def getColor(c, ambient, object, objects, lights, ray, hitP, max_depth, level):
+def getColor(ambient, obj, objects, lights, ray, hitP, max_depth, level):
+    hitP += 1e-2 * obj.getNormal(hitP)
+
     # Ambiant and Emission Calculations
-    color = calcEmissionColor(object) + calcAmbientColor(c, ambient, object)
+    color = calcEmissionColor(obj) + calcAmbientColor(ambient, obj)
     
     # Diffuse & Specular Calculation
     for light in lights:
-        sj = calcShadowFactor(light, objects, hitP)
-        color = color + sj * (calcDiffuseColor(c, object, objects, hitP, ray, light) + calcSpecularColor(c, object, objects,hitP,ray,light))
+        sj = calcShadowFactor(light, obj, objects, hitP)
+        if sj == 0.0:
+            continue
+        color += sj * (calcDiffuseColor(obj, hitP, light) + calcSpecularColor(obj,hitP,ray,light))
     
     level += 1
     
@@ -46,46 +52,62 @@ def getColor(c, ambient, object, objects, lights, ray, hitP, max_depth, level):
         return color
     
     # Reflective and Refractive components
-    #r_ray = ConstructReflectiveRay(ray, objects, hitP)
-    #r_hit = r_ray.nearest_intersected_object(objects)
-    #color += k_r * getColor(objects, r_ray, r_hit, level)
-    #t_ray = ConstructRefractiveRay(ray, objects, hitP)
-    #t_hit = t_ray.nearest_intersected_object(objects)
-    #color += k_t * getColor(objects, t_ray, t_hit, level)
-    
+    r_ray = ConstructReflectiveRay(ray, obj, hitP)
+    r_intersect = r_ray.nearest_intersected_object(objects)
+    if r_intersect is not None:
+        t, r_obj = r_intersect
+        r_hit = r_ray.origin + t * r_ray.direction
+        color += r_obj.reflection * getColor(ambient, r_obj, objects, lights, r_ray, r_hit, max_depth, level)
+   
+    t_ray = ConstructRefractiveRay(ray, hitP)
+    t_intersect = t_ray.nearest_intersected_object(objects)    
+    if t_intersect is not None:
+        t, t_obj = t_intersect
+        t_hit = t_ray.origin + t * t_ray.direction
+        color += t_obj.refraction * getColor(ambient, t_obj, objects, lights, t_ray, t_hit, max_depth, level)
     return color
 
-def calcEmissionColor(object):
-    if isinstance(object, LightSource):
-        return 1
+def calcEmissionColor(obj):
+    if isinstance(obj, LightSource):
+        return 1.0
     else:
-        return 0
+        return 0.0
     
-def calcAmbientColor(c, ambient, object):
-    return object.ambient[c] * ambient[c]
+def calcAmbientColor(ambient, obj):
+    return obj.ambient * ambient
 
-def calcShadowFactor(light, objects, hitP):
+def calcShadowFactor(light, obj, objects, hitP):
     ray = light.get_light_ray(hitP)
-    if ray.nearest_intersected_object(objects) is None:
-        return 1
+    light_distance = light.get_distance_from_light(hitP)
+    obj_intersect = ray.nearest_intersected_object(objects)
+    if obj_intersect is None:
+        return 1.0
     
-    _, intersected_obj = ray.nearest_intersected_object(objects)
+    t, _ = obj_intersect
 
-    if intersected_obj == light:
-        return 1
+    if t < light_distance:
+        return 0.0
     else:
-        return 0
+        return 1.0
 
-def calcDiffuseColor(c, object, objects, hitP, ray, light):
+def calcDiffuseColor(obj, hitP, light):
     vec_l = light.get_light_ray(hitP).direction
-    vec_n = object.normal
-    return object.diffuse[c] * light.get_intensity(hitP)[c] * np.dot(vec_n, vec_l)
+    vec_n = obj.getNormal(hitP)
+    dot_nl = max(np.dot(vec_n, vec_l), 0.0)
+    return obj.diffuse * light.get_intensity(hitP) * dot_nl
 
-def calcSpecularColor(c, object, objects, hitP, ray, light):
-    l_hat = reflected(ray.direction, object.normal)
-    v = ray.direction
-    return object.specular[c] * light.get_intensity(hitP)[c] * np.dot(v, l_hat) ** object.shininess
+def calcSpecularColor(obj, hitP, ray, light):
+    vec_l = light.get_light_ray(hitP).direction
+    l_hat = reflected(-vec_l, obj.getNormal(hitP))
+    v = normalize(-ray.direction)
+    return obj.specular * light.get_intensity(hitP) * (np.dot(l_hat, v) ** obj.shininess)
     
+def ConstructReflectiveRay(ray, obj, hitP):
+    return Ray(hitP, reflected(ray.direction, obj.getNormal(hitP)))
+
+def ConstructRefractiveRay(ray, hitP):
+    return Ray(hitP, ray.direction)
+
 # Write your own objects and lights
 # TODO
 def your_own_scene():
